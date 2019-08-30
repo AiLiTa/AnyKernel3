155fda45507bd1514cc543a086a611bdf759a19f
AiLiTa <tantio86@gmail.com>
2019-08-30 14:02:17 +0700

    returned it

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Titan Kernel '$(cat ../titan_kernel/release)' for Zenfone Max Pro M2 by @titanotom
+kernel.string=tiTan Kernel for Zenfone Max Pro M2 by @titanotom
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
